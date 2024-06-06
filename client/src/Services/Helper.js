fee01fd141cb23c25016af4798232df899b00dce
Tanishq Jain <tanishq.jain19@gmail.com>
2024-06-06 16:21:32 +0530

    changed url

--- client/src/Services/Helper.js
+++ client/src/Services/Helper.js
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "http://localhost:4000";
-// export const BACKEND_URL = "https://lostnfoundhub-server.onrender.com";
+// export const BACKEND_URL = "http://localhost:4000";
+export const BACKEND_URL = "https://lostinvit-bhopal.onrender.com";
